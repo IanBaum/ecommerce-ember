00ee35e92671884545d354dff1093d6abe09dd71
ian baum <ian@ianbaum.com>
2016-11-02 15:55:57 -0400

    add bootstrap and half functional details for cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart:Ember.inject.service(),
+  itemIsClicked:false,
+
+  actions:{
+    showItemDetails(){
+      this.set('itemIsClicked', true);
+    }
+  }
 });
